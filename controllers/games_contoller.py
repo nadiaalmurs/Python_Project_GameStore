d7e24d312b109b3cfb0cb07d55cb8c9b3af4df8e
Nadia Al-Murshedy <nadia@Nadias-MacBook-Air.local>
2022-05-27 02:52:30 +0100

    add game

--- controllers/games_contoller.py
+++ controllers/games_contoller.py
@@ -17,3 +17,24 @@ def delete_game(id):
     game_repository.delete(id)
     return redirect("/games")
 
+@games_blueprint.route("/games/new", methods=['GET'])
+def new_game():
+    games = game_repository.select_all()
+    return render_template("games/new.html", all_games = games)
+
+@games_blueprint.route("/games", methods=['POST'])
+def create_game():
+    title = request.form['title']
+    developer_id = request.form['developer']
+    genre = request.form['genre']
+    description = request.form['description']
+    stock = request.form['stock']
+    buy = request.form['buy']
+    sell = request.form['sel']
+
+    developer = developer_repository.select(developer_id)
+
+    game = Game(title, genre, description, stock, buy, sell, developer)
+
+    game_repository.save(game)
+    return redirect("/games")
