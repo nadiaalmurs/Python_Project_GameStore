09eaadf421b391cc6bb947b0d2da432f43182dab
Nadia Al-Murshedy <nadia@Nadias-MacBook-Air.local>
2022-05-27 13:49:26 +0100

    mvp completed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from controllers.games_contoller import games_blueprint
+from controllers.games_controller import games_blueprint
 from controllers.developers_controller import developers_blueprint
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.register_blueprint(games_blueprint)
 app.register_blueprint(developers_blueprint)
 
-app.route('/')
+@app.route('/')
 def home():
     return render_template('index.html')
 
