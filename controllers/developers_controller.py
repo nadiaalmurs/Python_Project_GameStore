826bcfa9f0cfda63027bfc0b20211805ffd6bd9d
Nadia Al-Murshedy <nadia@Nadias-MacBook-Air.local>
2022-05-27 11:11:36 +0100

    add developer

--- controllers/developers_controller.py
+++ controllers/developers_controller.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template, request
+from models.developer import Developer
 from repositories import developer_repository
 from repositories import game_repository
 from models.game import Game
@@ -17,3 +18,19 @@ def delete_developer(id):
     developer_repository.delete(id)
     return redirect("/developers")
 
+@developers_blueprint.route("/developers/new", methods=['GET'])
+def new_game():
+    developers = developer_repository.select_all()
+    return render_template("developers/new.html", all_developers = developers)
+
+@developers_blueprint.route("/developers", methods=['POST'])
+def create_game():
+    name = request.form['name']
+    shipping_price = request.form['shipping_price']
+    shipping_time = request.form['shipping_time']
+
+    developer = Developer(name, shipping_price, shipping_time)
+
+    developer_repository.save(developer)
+
+    return redirect("/developers")
